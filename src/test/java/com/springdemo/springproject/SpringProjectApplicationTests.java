c81ab3c6dd25ba3a0810abba9214ac0ae97c3dcb
Mehdii909 <76838172+Mehdii909@users.noreply.github.com>
2022-08-03 23:22:56 +0100

    update the controller

--- src/test/java/com/springdemo/springproject/SpringProjectApplicationTests.java
+++ src/test/java/com/springdemo/springproject/SpringProjectApplicationTests.java
@@ -1,27 +1,13 @@
 package com.springdemo.springproject;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SpringProjectApplicationTests {
 
-    @LocalServerPort
-    private int port;
-
-    @Autowired
-    private TestRestTemplate testRestTemplate;
-
     @Test
-    public void shouldPassIfStringMatches() {
-        assertEquals("Hello World from Spring Boot",
-                testRestTemplate.getForObject("http://localhost:"+port+"/",
-                        String.class));
+    void contextLoads() {
     }
-}
 
+}
